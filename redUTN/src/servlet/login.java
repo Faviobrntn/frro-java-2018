efb3adfd298b8388f9d73b0d7d735b7321db3f31
Favio <faviobarnatan@gmail.com>
2018-08-30 22:13:35 -0300

    primer commit

---
+++ redUTN/src/servlet/login.java
@@ -0,0 +1,62 @@
+package servlet;
+
+import java.io.IOException;
+import java.util.Map.Entry;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.catalina.tribes.group.interceptors.TwoPhaseCommitInterceptor.MapEntry;
+
+/**
+ * Servlet implementation class login
+ */
+@WebServlet({ "/login", "/Login", "/LOGIN" })
+public class login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		for(Entry<String, String[]> entry : request.getParameterMap().entrySet()){
+			System.out.println(entry.getKey()+" - "+entry.getValue()[0]);
+		}
+		//System.out.println(request.getParameter(""));
+		if(request.getParameter("alta") != null) {
+			System.out.print("Esta es un Alta");
+		}
+		if(request.getParameter("baja") != null) {
+			System.out.print("Esta es una Baja");
+		}
+		if(request.getParameter("edit") != null) {
+			System.out.print("Esta es una Modificación");
+		}
+		if(request.getParameter("search") != null) {
+			System.out.print("Esta es una Busqueda");
+		}
+		
+	}
+
+}
---
+++ redUTN/src/servlet/testLogin.java
@@ -0,0 +1,59 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class testLogin
+ */
+@WebServlet({ "/testLogin", "/TESTLOGIN", "/testlogin", "/TESTlogin", "/testLOGIN" })
+public class testLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public testLogin() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// request.getParameter("email") --> leo el name del form
+		
+		// request.getSession()  ---> SESSION: Si no existe la crea
+		/* CONDICIONES para guardar un objeto en SESION: 
+		 * tiene que ser un JAVA BINS
+		 * 		ser serializable (public cass User implements Serializable (){} ) 
+		 *  	tenes un construcctor publico y sin parametros
+		 *  	getter y setter para todos sus atributos
+		 * 
+		 * request.getSession().setAttribute("usuario", u);  User u = new User(); 
+		 */
+		
+		/*
+		 * request.getRequestDispatcher("WEB-INF/UserManagement.jsp")  
+		 * --> sirve para evitar accesos ilegales, todo lo que este aca esta protegido y solo se puede acceder por servlent
+		 */
+		System.out.println(request.getParameter("email"));
+		//doGet(request, response);
+		request.getRequestDispatcher("WEB-INF/login.html").forward(request, response);
+	}
+
+}
