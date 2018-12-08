495e4fac48047c184767edcb40482dfeb6df0893
nawealvarez <37459045+nawealvarez@users.noreply.github.com>
2018-12-08 18:53:32 -0300

    cambios mail

--- AVSA/src/util/Emailer.java
+++ AVSA/src/util/Emailer.java
@@ -3,30 +3,29 @@ package util;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+
 import javax.mail.*;
 import javax.mail.internet.*;
 
 public class Emailer {
-	
-	public static Emailer instance;
-	
+
+	private static Emailer instance;
 	private Properties props;
 	
-	public static Emailer getInstance(){
-		 /*Es un singleton, lo crea una sola vez*/
-		if (instance==null){
+	public static Emailer getInstance()throws Exception{
+		if(instance==null){
 			instance=new Emailer();
 		}
 		return instance;
 	}
 	
-	private Emailer() {
+	
+    private Emailer() throws Exception{
 		
 		InputStream inputStream=getClass().getClassLoader().getResourceAsStream("app.properties");
 		try {
 			props = new Properties();
 			props.load(inputStream);
-			
 			/*
 			 * props.put("mail.smtp.auth", "true");
 			 * props.put("mail.smtp.starttls.enable", "true");
@@ -35,14 +34,16 @@ public class Emailer {
 			 * props.put("mail.username", "somemail@gmail.com");
 			 * props.put("mail.password","someRandomwPassword");
 			 */
+			//esto lo sacamos del archivo app.properties y es mas facil ademas si un dia queremos cambiarlo
+			//tocamos el archivo nomas
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			throw e;
 		}
 		
 	}
-	
-	public void send(String to, String subject, String body){
+    
+    /*funcion para mandar mail*/
+    public void send(String to, String subject, String body) throws Exception{
 
 		Session session = Session.getInstance(props,
 		  new javax.mail.Authenticator() {
@@ -50,22 +51,28 @@ public class Emailer {
 				//return new PasswordAuthentication(username, password);
 				return new PasswordAuthentication(props.getProperty("mail.username"), props.getProperty("mail.password"));
 			}
-		  });
+		  });//arma la sesion 
 
 		try {
 
 			Message message = new MimeMessage(session);
 			message.setFrom(new InternetAddress(props.getProperty("mail.username")));
+			
+			//despues viene el recipients que setea quien lo va a recibir,recibe dos cosas,que tipo de recipien es y un arraylist
+			//de direcciones de internet o una sola,en el InternetAddress.parse(to) los convierte en un arraylist de internetaddress
+			//despues le digo que tipo de recipient es, en este caso To (o sra drstinatario)
 			message.setRecipients(Message.RecipientType.TO,
-				InternetAddress.parse(to)); //"aaa@gmail.com"
+			InternetAddress.parse(to)); //"adrianmeca@gmail.com"
 			message.setSubject(subject); //"Testing Subject"
+			//subject es el tema,el asunto de arriba
 			message.setText(body); //"Dear Mail Crawler,\n\n No spam to my email, please!"
 
 			Transport.send(message);
 
 		} catch (MessagingException e) {
-			throw new RuntimeException(e);
+			//throw new RuntimeException(e);
+			throw e;
 		}
+		
 	}
-
 }
